f80b01f86a511f954b0ee5a35864cceb71cb1225
arta fallahpoor <102357381+artafps@users.noreply.github.com>
2025-10-01 00:13:21 +0330

    Add Card UI component and MergeSortVisualizer import
    Introduces a new Card component with subcomponents for UI layout in src/components/ui/card.tsx. Also updates main.tsx to import and render MergeSortVisualizer.

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Button } from './components/ui/button.tsx'
+import MergeSortVisualizer from './common/MergeSortVisualizer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <Button variant="secondary">Click me</Button>
+    <MergeSortVisualizer></MergeSortVisualizer>
   </StrictMode>,
 )
