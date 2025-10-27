9060d78ba560e3cef9b4057194f611ecc02a7274
arta fallahpoor <102357381+artafps@users.noreply.github.com>
2025-10-27 15:09:38 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  base: "/algorithms-in-action/",
+  base: "https://artafps.github.io/algorithms-in-action/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
